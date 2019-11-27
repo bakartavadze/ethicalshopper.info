a93dee93fc101fff2cf7b8cd2c50fe76adeed0a8
7iauhn <7iauhn@gmail.com>
2019-11-26 22:16:35 -0500

    get_data() working

--- parse.py
+++ parse.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
+from collections import defaultdict
+from copy import deepcopy
 import os
 import time
+import pickle
 
 class GoodOnYou:
     def __init__(self):
@@ -51,38 +54,122 @@ class GoodOnYou:
                 if last_count==page_length:
                     match=True
 
-    # returns all brands in a single category 
-    def _get_brands_of(self, category) -> list:
+    # returns all brand tags in a single category 
+    # 'homepage_url/brand/<brand_tag>/'
+    def _get_brand_tags_in(self, category) -> list:
         url = self._homepage_url + 'categories/' + category
         self._driver.get(url)
         self._scroll_bottom()
         html = BeautifulSoup(self._driver.page_source, 'html.parser')
-        brands = [a['href'].split('brand/')[1] for a in html.find_all('a', href=True) if 'brand' in a['href']]
+        brand_tags = [a['href'].split('brand/')[1] for a in html.find_all('a', href=True) if 'brand' in a['href']]
         # todo: delete the print statement
-        print('size of', category, 'is', len(brands))
-        return brands
+        print('size of', category, 'is', len(brand_tags))
+        return brand_tags
     
-    # returns all brands in all categories (union of all brands)
-    def get_brands(self, categories, sort=False) -> list:
+    # returns all brand tags in all categories (union of all brand tags)
+    def get_every_brand_tag(self, categories, sort=False) -> list:
         s = set()
         for category in categories:
-            brands_in_category = self._get_brands_of(category)
-            while not brands_in_category:
-                brands_in_category = self._get_brands_of(category)
-            s = s.union(brands_in_category)
-        brands = list(s)
+            tags_in_category = self._get_brand_tags_in(category)
+            while not tags_in_category:
+                tags_in_category = self._get_brand_tags_in(category)
+            s = s.union(tags_in_category)
+        brand_tags = list(s)
         if sort:
-            brands.sort()
-        return brands
+            brand_tags.sort()
+        return brand_tags
+
+    def _get_brand_website(self, html):
+        brand_website = [a['href'].split('?')[0] for a in html.find_all('a', href=True) if 'utm_source=GoodOnYou' in a['href']]
+        if brand_website:
+            return brand_website[-1]
+        return None
+    
+    def _get_brand_name(self, html):
+        brand_name = [h1.text.strip() for h1 in html.find_all('h1')]
+        if brand_name:
+            return brand_name[-1]
+        return None
+    
+    def _get_brand_rating(self, html):
+        brand_rating = [span.text.split('Rated: ')[-1] for span in html.find_all('span') if 'Rated' in span.text]
+        if brand_rating:
+            return brand_rating[0]
+        return None
+    
+    def _get_brand_price(self, html):
+        brand_price = [span.text.split(' ')[-1] for span in html.find_all('span') if 'Price' in span.text]
+        if brand_price:
+            return brand_price[0]
+        return None
+
+    def _get_brand_message(self, html):
+        brand_message = [h4.text.strip() for h4 in html.find_all('h4')]
+        if brand_message:
+            return brand_message[-1]
+        return None
+
+    def _get_brand_planet_rating(self, html):
+        all_ratings = [span.text[0] for span in html.find_all('span') if 'out of 5' in span.text]
+        if len(all_ratings) == 3:
+            return all_ratings[0]
+        return None
+
+    def _get_brand_people_rating(self, html):
+        all_ratings = [span.text[0] for span in html.find_all('span') if 'out of 5' in span.text]
+        if len(all_ratings) == 3:
+            return all_ratings[1]
+        return None
+
+    def _get_brand_animals_rating(self, html):
+        all_ratings = [span.text[0] for span in html.find_all('span') if 'out of 5' in span.text]
+        if len(all_ratings) == 3:
+            return all_ratings[2]
+        return None
+
+    # maps brand name to brand data
+    # if no brand name is found then brand name becomes 'NA ' + brand_tag
+    def _get_brand_name_to_data_mapping(self, brand_tag) -> dict:
+        GoodOnYou_url = self._homepage_url + 'brand/' + brand_tag
+        self._driver.get(GoodOnYou_url)
+        html = BeautifulSoup(self._driver.page_source, 'html.parser')
+
+        brand_name = self._get_brand_name(html)
+        if not brand_name:
+            brand_name = 'NA ' + brand_tag
+        data = {}
+        mapping = {brand_name:data} 
+        data['rating'] = self._get_brand_rating(html)
+        data['price'] = self._get_brand_price(html)
+        data['planet_rating'] = self._get_brand_planet_rating(html)
+        data['people_rating'] = self._get_brand_people_rating(html)
+        data['animals_rating'] = self._get_brand_animals_rating(html)
+        data['message'] = self._get_brand_message(html)
+        data['GoodOnYou_url'] = GoodOnYou_url
+        
+        return mapping
+    
+    def data(self, brand_tags: list):
+        if not isinstance(brand_tags, list):
+            raise ValueError('Brand tags must be provided in a list')
+        data = {}
+        for brand_tag in brand_tags:
+            mapping = self._get_brand_name_to_data_mapping(brand_tag)
+            key = next(iter(mapping))
+            data[key] = deepcopy(mapping[key])
+            time.sleep(2)
+        return data
 
 
 
-# website = GoodOnYou()
-# categories = website.get_categories()
-# print(categories)
 
-# brands = website.get_brands(categories)
-# print('total brands =', len(brands))
-# print(brands)
+website = GoodOnYou()
 
-# 1121 brands in total
+with open('brand_tags.pkl', 'rb') as f:
+    brands = pickle.load(f)
+data = website.data(brands[:5])
+print(data.keys())
+print(data)
+
+# directory_url = 'https://directory.goodonyou.eco/brand/ftla-apparel'
+# directory_url = 'https://directory.goodonyou.eco/brand/dolce-and-gabbana'
