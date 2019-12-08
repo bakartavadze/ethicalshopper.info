ef71179e58366766b84d135ee0ccb06ffc241f99
7iauhn <7iauhn@gmail.com>
2019-12-07 22:06:44 -0500

    extension fully functional

--- extension/background.js
+++ extension/background.js
@@ -1,22 +1,30 @@
 chrome.runtime.onInstalled.addListener(function() {
-    const url = chrome.runtime.getURL('json.txt');
-        fetch(url)
+    const names_to_data_mapping = chrome.runtime.getURL('brand_names_to_data.txt');
+        fetch(names_to_data_mapping)
         .then((response) => response.json())
         .then((json) => {
             chrome.storage.local.set({names_to_data: json}, function() {
             });
         });
+    const urls_to_names_mapping = chrome.runtime.getURL('brand_urls_to_brand_names.txt');
+        fetch(urls_to_names_mapping)
+        .then((response) => response.json())
+        .then((json) => {
+            chrome.storage.local.set({urls_to_names: json}, function() {
+            });
+        });
     
 
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          })
-          ],
-          
-              actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-      });
+        chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+            chrome.declarativeContent.onPageChanged.addRules([{
+              conditions: [new chrome.declarativeContent.PageStateMatcher({
+              })
+              ],
+                  actions: [new chrome.declarativeContent.ShowPageAction()]
+            }]);
+          });
 
 });
+
+
 
